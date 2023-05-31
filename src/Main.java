40d89b1a337530fc2dc4e61cf8ed3ac90b048a03
maxzlobin81 <maxzlobin@gmail.com>
2023-05-30 20:18:33 -0500

    minor change

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         for (int i = 0; i < 10; i++) {
             System.out.println("Hello world!");
-            Thread.sleep(2000);
+            Thread.sleep(1000);
         }
     }
 }
